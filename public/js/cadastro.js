35a6686fb3567ecc8caf1197802e102ea19b29d6
GabrielMarRib <gabriel.mribeiro@sptech.school>
2025-11-14 00:21:40 -0300

    Atualização e inicativa para implementação co m o BD

---
+++ public/js/cadastro.js
@@ -0,0 +1,118 @@
+
+
+function Alerta(Msg, Cor){
+    Opacidade = IDcontaineralerta.style.opacity
+    if(Opacidade == 0){
+        IDcontaineralerta.style.opacity = "100";
+        mensagem_erro.style.color = Cor
+        mensagem_erro.innerHTML = Msg
+        setInterval(() => {IDcontaineralerta.style.opacity = "0"}, 3000)
+    }else{
+        IDcontaineralerta.style.opacity = "0";
+
+    }
+
+}
+
+
+
+ let UsuariosCadastrados = [];
+
+
+function FnCadastrar(){
+    
+    var UserName = Input_UserName.value;
+    var userEmail = Input_UserEmail.value;
+    var UserSenha = Input_UserSenha.value;
+    var UserConfirmaSenha = Input_UserConfirmarSenha.value;
+    var TermosECondicoes = Input_Termos.checked
+
+    if(TermosECondicoes == false){
+    Alerta("Termos&Condições não aceitos!", "#b09000");    
+      return false;
+    }
+    if(UserSenha != UserConfirmaSenha){
+        Alerta("As senhas estão diferentes", "#b09000");    
+        return false
+    }
+    if (UserName == "" || userEmail == "" || UserSenha == "" || UserConfirmaSenha == ""){
+      Alerta("Algum dos campos não foi preenchido!", "#b09000");  
+      return false;
+    } else {
+      IDcontaineralerta.style.opacity = "0";
+    }
+
+
+     
+    // Verificando se o código de ativação é de alguma empresa cadastrada
+    for (let i = 0; i < UsuariosCadastrados.length; i++) {
+      if (UsuariosCadastrados[i].email == userEmail) {
+        Alerta("Email já cadastrado!", "red")
+        return
+      }
+    }
+
+    // Enviando o valor da nova input
+    fetch("/usuarios/cadastrar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // crie um atributo que recebe o valor recuperado aqui
+        // Agora vá para o arquivo routes/usuario.js
+        nomeServer: UserName,
+        emailServer: userEmail,
+        senhaServer: UserSenha
+      }),
+    })
+      .then(function (resposta) {
+        console.log("resposta: ", resposta);
+
+        if (resposta.ok) {
+             Alerta("Cadastro realizado com sucesso! Redirecionando para tela de Login...", "green");  
+
+          setTimeout(() => {
+            window.location = "./login.html";
+          }, "2000");
+
+        } else {
+          throw "Houve um erro ao tentar realizar o cadastro!";
+        }
+      })
+      .catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+        finalizarAguardar();
+      });
+
+    return false;
+  }
+
+  // Listando usuarios cadastradas 
+  function listar() {
+    
+    fetch("/usuarios/listar", {
+      method: "GET",
+    })
+      .then(function (resposta) {
+          resposta.json().then((usuario) => {
+              
+          usuario.forEach((usuario) => {
+            UsuariosCadastrados.push(usuario);
+            
+            console.log("UsuariosCadastrados")
+        });
+
+        console.log(UsuariosCadastrados)
+        });
+      })
+      .catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+      });
+  }
+
+
+  
+  
+
+
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -10,13 +10,23 @@ function autenticar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha, fkEmpresa) {
+function cadastrar(nome, email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, fk_empresa) VALUES ('${nome}', '${email}', '${senha}', '${fkEmpresa}');
+        INSERT INTO usuario (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+function listar() {
+    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucaoSql = `
+        SELECT email FROM usuario;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -24,5 +34,6 @@ function cadastrar(nome, email, senha, fkEmpresa) {
 
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar,
+    listar
 };
